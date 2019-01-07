ab744b9874abbde1258547fb2b9bafce410b0b23
Davideczech <45853218+Davideczech@users.noreply.github.com>
2019-01-07 14:51:48 +0100

    Add files via upload

---
+++ ex_1_variables.py
@@ -0,0 +1,13 @@
+'''
+Create a Python script that has three variables: ip_addr1, ip_addr2, ip_addr3
+(representing three corresponding IP addresses). Print these three variables to standard output using a single print statement.
+Make your print statement compatible with both Python2 and Python3.*/
+'''
+
+from __future__ import print_function
+
+ip_addr1 = "10.0.0.1/24"
+ip_addr2 = "11.0.0.1/24"
+ip_addr3 = "12.0.0.1/24"
+
+print(ip_addr1, ip_addr2, ip_addr3)
---
+++ stdin.py
@@ -0,0 +1,3 @@
+test_string = "alfa_centauri"
+
+print(test_string.upper())
