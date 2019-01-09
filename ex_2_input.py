fbd75c3d596a89cb8743a02406dada5faf099218
Davideczech <45853218+Davideczech@users.noreply.github.com>
2019-01-09 10:18:00 +0100

    Delete ex_2_input.py

--- ex_2_input.py
+++
@@ -1,5 +0,0 @@
-''' 2. Prompt a user to enter in an IP address from standard input.
-    Read the IP address in and break it up into its octets. Print out the octets in decimal, binary, and hex.
- '''
-
- 
